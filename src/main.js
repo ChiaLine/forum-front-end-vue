cec58f57c98379ca8c17d45e1114358da127a8b5
egg <egg810207@gmail.com>
2022-02-26 14:20:36 +0800

    新增優化 CSS 設定：全域樣式、針對各個 component 調整樣式

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 // 整個專案JavaScript 的進入點
-
 // Vue.js 本身
 import Vue from 'vue'
-
 // 專案的根元件，專案本身
 import App from './App.vue'
-
 import router from './router'
 import store from './store'
+// 載入 全域樣式 css 檔
+import './assets/application.css'
 
 Vue.config.productionTip = false
 
